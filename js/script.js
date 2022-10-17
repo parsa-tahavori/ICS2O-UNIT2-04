92ded0a1aac7f91b059f54040f43c061cd244513
parsa-tahavori <parsa-tahavori@users.noreply.github.com>
2022-10-17 18:34:50 +0000

    Prettified Code!

--- js/script.js
+++ js/script.js
@@ -4,18 +4,19 @@
 // Created on: Sep 2020
 // This file contains the JS functions for index.html
 
-'use strict'
+"use strict"
 /**
  * This function calculates area and perimeter of triangle.
  */
-function calculate () {
+function calculate() {
   // input
-  var base = parseInt(document.getElementById('base-of-triangle').value)
-  var hight = parseInt(document.getElementById('hight-of-triangle').value)
+  var base = parseInt(document.getElementById("base-of-triangle").value)
+  var hight = parseInt(document.getElementById("hight-of-triangle").value)
   // process
   const area = hight * base
-  const perimeter = (hight + base)
+  const perimeter = hight + base
   // output
-  document.getElementById('area').innerHTML = 'Area is: ' + area +' cm²'
-  document.getElementById('perimeter').innerHTML = 'Perimeter is: ' + perimeter +' cm'
+  document.getElementById("area").innerHTML = "Area is: " + area + " cm²"
+  document.getElementById("perimeter").innerHTML =
+    "Perimeter is: " + perimeter + " cm"
 }
